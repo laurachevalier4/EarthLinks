3ba0f643c7e00747525ad28e61622fee3a56287a
Laura <laurachevalier4@yahoo.com>
2016-09-25 07:57:50 -0400

    get current user

---
+++ fb.js
@@ -0,0 +1,25 @@
+var userID;
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '307079902999566',
+    xfbml      : true,
+    version    : 'v2.7'
+  });
+};
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "//connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
+// This is called with the results from from FB.getLoginStatus().
+
+// Here we run a very simple test of the Graph API after login is
+// successful.  See statusChangeCallback() for when this call is made.
+function testAPI() {
+  console.log('Welcome!  Fetching your information.... ');
+  FB.api('/me', function(response) {
+    console.log('Successful login for: ' + response.name);
+    userID = response.userID;
+}
